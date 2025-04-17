aa0adf7ce5c73992fcfde021d81f1e0708df8629
marcelarie <googlillo@gmail.com>
2025-04-17 15:12:48 +0200

    add cachix nixConfig again

--- flake.nix
+++ flake.nix
@@ -1,13 +1,6 @@
 {
   description = "NixOS and Home Manager configuration";
 
-  nixConfig = {
-    extra-substituters = ["https://marcelarie.cachix.org"];
-    extra-trusted-public-keys = [
-      "marcelarie.cachix.org-1:loFQMIgWqiIgfRixHOrEwbGADvFYu8RJXF6jqL0HUy8="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
@@ -30,6 +23,17 @@
       config.allowUnfree = true;
     };
   in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        git
+        nix-prefetch
+        cachix
+      ];
+      shellHook = ''
+        echo "🐚  Dev shell for ${username} on ${system} ready!"
+        export EDITOR=nvim
+      '';
+    };
     nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
       inherit system;
       modules = [
