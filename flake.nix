84bd5fdda4391efb79b00690701e06a52f1da320
marcelarie <googlillo@gmail.com>
2026-01-13 15:43:27 +0100

    update

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgsStable.url = "github:NixOS/nixpkgs/nixos-25.11";
     nu-alias-converter.url = "github:marcelarie/nu-alias-converter";
+    nur.url = "github:nix-community/NUR";
     lsv = {
       url = "path:./packages/lsv";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -84,6 +85,7 @@
     pulseaudio-next-output,
     git-commit-search,
     zen-browser,
+    nur,
     ...
   } @ inputs: let
     hyprlandInputs = inputs.hyprland;
@@ -104,6 +106,7 @@
        overlays = [
            # hyprlandInputs.overlays.default
            # hyprlandPlugins.overlays.default
+           nur.overlays.default
            (import ./overlays/neovim-nightly.nix {inherit inputs;})
           (final: prev: {tmex = tmexPkg;})
         (final: prev: {nuit = nu-alias-converter.packages.${system}.default;})
