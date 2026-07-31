46de17d259623ddad373f33b1e178e66a2daa104
themarcel <code@marcel.cool>
2026-07-31 13:33:57 +0200

    update vendor hash

--- overlays/mautrix-whatsapp.nix
+++ overlays/mautrix-whatsapp.nix
@@ -4,7 +4,8 @@ final: prev: {
       owner = "marcelmanz";
       repo = "whatsapp";
       rev = "keep-deleted-messages";
-      hash = final.lib.fakeSha; # build once, paste the got hash, rebuild
+      sha256 = "sha256-bvnp95Pz2UPZt2tPRWRAPdeNTk9tKo2YVd2YSwuAC/Q=";
     };
+    vendorHash = "sha256-U67qtG+J7iXq1+YApwWj1P0S9Rp0X5fMnXwiY8/8LOw=";
   });
 }
